016c970bc8683cea83a9e99918063d20b37b0608
Hector <rankecillo@gmail.com>
2024-08-12 14:35:51 -0600

    Added pagination

--- js/script.js
+++ js/script.js
@@ -5,8 +5,12 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
+    // Resgister your key and enter here
+    // ONly use this for development of very small projects. Normally you should store your key and makes requests from a server.
+
     apiKey: 'b1fdd2e9eda3c8eca092c94054400a95',
     apiUrl: 'https://api.themoviedb.org/3/',
   },
@@ -254,7 +258,12 @@ async function search() {
   global.search.term = urlParams.get('search-term');
 
   if (global.search.term !== '' && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
     if (results.length === 0) {
       showAlert('No results found');
       return;
@@ -268,6 +277,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
   results.forEach((result) => {
     const div = document.createElement('div');
     div.classList.add('card');
@@ -304,7 +318,52 @@ function displaySearchResults(results) {
             </p>
           </div>
         `;
+    document.querySelector('#search-results-heading').innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
     document.querySelector('#search-results').appendChild(div);
+  });
+  displayPagination();
+}
+
+// Create and display pagination for search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable Prev button if first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if last page
+
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev page
+
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
@@ -358,8 +417,6 @@ function initSwiper() {
 // Fetch data from TMBD API
 
 async function fetchAPIData(endpoint) {
-  // Resgister your key and enter here
-  // ONly use this for development of very small projects. Normally you should store your key and makes requests from a server.
   const API_KEY = global.api.apiKey;
   const API_URL = global.api.apiUrl;
 
@@ -385,7 +442,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&languaje=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&languaje=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
