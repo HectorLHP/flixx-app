6d10c53f6c37f74d0a197105ec668a8dcee4c77c
Hector <rankecillo@gmail.com>
2024-08-13 14:30:05 -0600

    Fixed avg vote on slider

--- js/script.js
+++ js/script.js
@@ -393,10 +393,14 @@ async function displaySlider() {
     div.classList.add('swiper-slide');
     div.innerHTML = `    
             <a href="movie-details.html?id=${movie.id}">
-              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+              <img src="https://image.tmdb.org/t/p/w500${
+                movie.poster_path
+              }" alt="${movie.title}" />
             </a>
             <h4 class="swiper-rating">
-              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+                1
+              )} / 10
             </h4>
           `;
 
